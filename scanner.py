e06ac48b4f7244f1aebe1ed7f1c469b4ccb356ab
adriansoghoian <adriansoghoian@gmail.com>
2014-10-31 12:20:11 -0400

    Working on network scan

--- scanner.py
+++ scanner.py
@@ -1,6 +1,5 @@
 __author__ = "Adrian Soghoian & Omar Ahmad"
 import os, sys, subprocess, socket
-from IPy import IP
 
 """
 This subsystem contains functionality to scan the local network for connected 
@@ -15,31 +14,32 @@ def scan_network(host):
 	os_strings = [ "Windows", "Apple", "IOS", "Linux", "Unknow" ]
 	win, apple, linux, ios, unknow = [ (os_strings[i], 0) for i in range(len(os_strings)) ]
 	os_count = [ win, apple, linux, ios, unknow ]
+	scan = subprocess.Popen(["nmap", "-sP", str(host)],stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+	scanlist = scan.split()
+	print scanlist
 
-    scan = subprocess.Popen(["nmap", "-PE","-PP","-PS21,22,23,25,80,443,3306,3389,8080", str(host)],stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
-    scanlist = scan.split()
+	for i, each in enumerate(os_count):
+		os = each[0]
+		count = scanlist.count(os)
 
-    for i, each in enumerate(os_count):
-    	os = each[0]
-    	count = scanlist.count(os)
+		if os == "Windows":
+			os_count[i] = (os, count)
+		elif os == "Apple":
+			os_count[i] = (os, count)
+		elif os == "Linux":
+			os_count[i] = (os, count)
+		else:
+			os_count[i] = (os, count)
 
-    	if os == "Windows":
-    		os_count[i][1] = count
-    	elif os == "Apple":
-    		os_count[i][1] = count
-    	elif os == "Linux":
-    		os_count[i][1] = count
-    	else:
-    		os_count[i][1] = count
+	return os_count
 
-    return os_count
-
-def scan_device():
+def scan_device(host):
 	"""
 	Conducts port scan of a specific device. 
 	"""
-	
-
+	ports = []
+	scan = subprocess.Popen(["nmap", str(host)],stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
+	scanlist = scan.split()
 
 	return True
 
@@ -47,7 +47,7 @@ def get_network_ip():
 	# s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 	# s.connect(("finch.com",80))
 	# ip = s.getsockname()[0]
-	ip = "192.168.0.0/24"
+	ip = "10.144.47.128"
 	# s.close()
 	return ip
 

5415c05a44cb3139f6df61a84210c211d29aaf61
adriansoghoian <adriansoghoian@gmail.com>
2014-11-25 17:09:45 -0500

    Cleaned up reference file.

--- reference.py
+++ reference.py
@@ -1,3 +1,6 @@
+"""
+These are used to parse the NEXT SERVICE FINGERPRINT fingerprint outputted by Nmap
+in case the OS fingerprint fails. The keywords are mapped to various known OSes that 
+sometimes fail Nmap detection. 
+"""
 OS_TYPES = { "apple": "Mac OS X" }
-
-PORT_REFERENCE = {}
